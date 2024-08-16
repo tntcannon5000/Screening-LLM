815a854fd90aba50557333c1d948861d6e77196a
tntcannon5000 <niranjan.kewalramani@outlook.com>
2024-08-16 02:19:45 +0100

    added exit condition to allow cmd-line exe to remain open while the results is read, and performed another "passing" interview to add to our records

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from src.modules.post_conversation import PostConversationProcessor
 
 def main():
     #timestamp = realtime_conversation.main()
-    postcon = PostConversationProcessor(1723670854)
+    postcon = PostConversationProcessor(1723769368)
     result = postcon.run()
 
     print(result)
+
+    input("Press enter to exit")
+
+    
 # Run the main.py file
 if __name__ == '__main__':
     main()
